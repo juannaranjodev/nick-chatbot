2ff5798bcf6ccac9e5855df7effcedcfc5aef46b
Nicholas Andrew Miller <nick.miller@govlawtech.com.au>
2019-01-30 21:50:34 +1100

    initial

---
+++ src/ApiEndpointsConfig.js
@@ -0,0 +1,11 @@
+﻿
+
+function getBotEndpoint() {
+    if (process.env.NODE_ENV === 'production') {
+        return "";
+    } else {
+        return "http://localhost:3978";
+    }
+}
+
+export {getBotEndpoint}
---
+++ src/Shell.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { createStore, createStyleSet } from 'botframework-webchat';
+
+import WebChat from './WebChat';
+
+import './fabric-icons-inline.css';
+import './MinimizableWebChat.css';
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+
+    
+    
+    this.handleFetchToken = this.handleFetchToken.bind(this);
+  
+    const store = createStore({}, ({ dispatch }) => next => action => {
+      if (action.type === 'DIRECT_LINE/CONNECT_FULFILLED') {
+        setTimeout(() => {
+          dispatch({
+            type: 'DIRECT_LINE/POST_ACTIVITY',
+            payload: {
+              activity: {
+                name: 'webchat/join',
+                type: 'event',
+                value: {
+                  language: window.navigator.language
+                }
+              }
+            }
+          });
+        }, 1000);
+      } else if (action.type === 'DIRECT_LINE/INCOMING_ACTIVITY') {
+        if (action.payload.activity.from.role === 'bot') {
+          this.setState(() => ({ newMessage: true }));
+        }
+      }
+
+      return next(action);
+    });
+
+
+
+    this.state = {
+       store,
+      styleSet: createStyleSet({
+        backgroundColor: 'white'
+      }),
+        token: null
+    };
+  }
+
+  async handleFetchToken() {
+    if (!this.state.token) {
+      const res = await fetch('/api/directlineToken', { method: 'POST' });
+      const { token } = await res.json();
+
+      this.setState(() => ({ token }));
+    }
+  }
+
+ 
+  render() {
+    const { state: {
+      store,
+      styleSet,
+      token
+    } } = this;
+
+    return (
+      <div className="minimizable-web-chat">
+        {
+          
+            <div
+              className={  'chat-box' }
+            >
+                  <div className= {'chat-box-header'}>
+                  <div className="chat-box-header-text">
+                   Ask "ANNA": Army, Navy 'n' Airforce
+                  </div>
+                  </div>
+               
+              <WebChat
+                className="react-web-chat"
+                onFetchToken={ this.handleFetchToken }
+                store={ store }
+                styleSet={ styleSet }
+                token={ token }
+              />
+            </div>
+        }
+      </div>
+    );
+  }
+}
